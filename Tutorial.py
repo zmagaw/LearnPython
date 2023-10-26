6874b8ba62c1b6ca4a627ff36cce811bbb2ec0aa
zach magaw <zmagaw@gmail.com>
2023-10-26 12:42:40 -0400

    Create Tutorial.py
    This is the first commit of this

---
+++ Tutorial.py
@@ -0,0 +1,7 @@
+#This the running program that will teach someone how to code python - but not just code python but use opther tooling and uibld process
+
+#should be a terminal like screen that allows for user input
+
+#create files
+
+#run a program 
